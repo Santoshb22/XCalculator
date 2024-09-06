a9e6294fd442efc8a9a704e75019b228b8c1e76d
Santoshb22 <sb289848@gmail.com>
2024-09-06 16:56:25 +0530

    XCalculator

---
+++ xcalculator/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import XCalculator from './Component/XCalculator/XCalculator'
+
+const App = () => {
+  return (
+    <div><XCalculator/></div>
+  )
+}
+
+export default App
---
+++ xcalculator/src/Component/XCalculator/XCalculator.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import "./XCalculator.css"
+const XCalculator = () => {
+    const [value, setValue] = useState(null);
+    const [input, setInput] = useState("");
+    function handleClick (buttonValue) {
+        if(buttonValue === "=") {
+            if(input.trim() === "") {
+                setValue("Error");
+            } else {
+                try {
+                    setValue(eval(input))
+                } catch {
+                    setValue("Error")
+                }
+            }
+        } else if(buttonValue === "C"){
+            setInput("");
+            setValue("");
+        } else {
+            setInput(prev => prev + buttonValue)
+        }
+    }
+
+  return (
+    
+    <div className='calculator'>
+        <h1>React Calculator</h1>
+
+        <div className="calculation">
+            <input value={input || 0} />
+            <p>{value}</p>
+        </div>
+        <div className="buttons">
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('C')}>C</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={() => handleClick('=')}>=</button>
+        <button onClick={() => handleClick('/')}>/</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default XCalculator
